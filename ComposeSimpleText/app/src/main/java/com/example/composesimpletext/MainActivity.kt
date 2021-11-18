d88dae8428a4b4d9cafa659578c5d791ea0e8932
양태웅 <didxodnd0422@gmail.com>
2021-11-18 22:09:42 +0900

    @Preview 어노테이션을 통한 Preview 화면 구성

--- ComposeSimpleText/app/src/main/java/com/example/composesimpletext/MainActivity.kt
+++ ComposeSimpleText/app/src/main/java/com/example/composesimpletext/MainActivity.kt
@@ -14,12 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeSimpleTextTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Android")
         }
     }
 }
@@ -29,6 +24,12 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
+
+/**
+@Preview 어노테이션을 통해 Preview 화면을 생성한다.
+Preview 화면의 내용은 @Preview 함수 안의 내용을 표시하기 때문에,
+실제 빌드 시 보이는 화면을 보기 위해서는 액티비티 안의 setContent 함수의 내용과 같게 해 주어야 한다.
+*/
 
 @Preview(showBackground = true)
 @Composable
