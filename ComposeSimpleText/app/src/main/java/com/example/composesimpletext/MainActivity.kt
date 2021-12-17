855ba1f5aabc7fc93963f0a62969710aa6b19459
양태웅 <didxodnd0422@gmail.com>
2021-12-17 18:17:09 +0900

    Alignment에 대해

--- ComposeSimpleText/app/src/main/java/com/example/composesimpletext/MainActivity.kt
+++ ComposeSimpleText/app/src/main/java/com/example/composesimpletext/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -19,6 +20,7 @@ import com.example.composesimpletext.ui.theme.ComposeSimpleTextTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MainScreen()
         }
@@ -31,13 +33,18 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-        Row {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
             ColoredSurface(color = Color.Blue)
             ColoredSurface(color = Color.White)
             ColoredSurface(color = Color.Red)
-            ColoredSurface(color = Color.Green)
-            ColoredSurface(color = Color.Green)
-            ColoredSurface(color = Color.Green)
+            ColoredSurface(color = Color.Blue)
+            ColoredSurface(color = Color.White)
+            ColoredSurface(color = Color.Red)
         }
     }
 }
@@ -48,26 +55,8 @@ fun ColoredSurface(color: Color) {
         color = color,
         modifier = Modifier
             .width(50.dp)
-            .fillMaxHeight()
+            .height(90.dp)
     ) {}
-}
-
-// @Composable 어노테이션을 통해 이 함수가 UI의 일부분임을 알려준다
-@Composable
-fun GreetingText(name: String) {
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier
-            /** Modifier -> 아래서 위로 적용된다!!! */
-            .fillMaxSize()
-            .fillMaxSize() // 안에 매개변수를 넣어 비율 조정 가능
-            .padding(24.dp)
-            .clickable {
-                // 클릭 시 이벤트
-            },
-        style = MaterialTheme.typography.h3,
-        fontWeight = FontWeight.Bold
-    )
 }
 
 @Preview(showBackground = true)
